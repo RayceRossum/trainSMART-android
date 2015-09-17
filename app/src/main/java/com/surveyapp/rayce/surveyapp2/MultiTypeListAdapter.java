608508f30a75d6e71514605028760e3b37f9b701
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-09-17 13:47:19 -0600

    Added create layout and moved creation of personToAssessment object out of editFrag

--- app/src/main/java/com/surveyapp/rayce/surveyapp2/MultiTypeListAdapter.java
+++ app/src/main/java/com/surveyapp/rayce/surveyapp2/MultiTypeListAdapter.java
@@ -212,8 +212,8 @@ public class MultiTypeListAdapter extends BaseAdapter {
                                 pToA.get_assessment_id() + " " +
                                 pageData.get(Integer.parseInt(me.getKey().toString())).get_assessments_questions_id()
                 );
-                DBHelper dbhelp = new DBHelper(context);
-                dbhelp.setEditPageRow(pToA, pageData.get(Integer.parseInt(me.getKey().toString())).get_assessments_questions_id(), me.getValue().toString());
+                //DBHelper dbhelp = new DBHelper(context);
+                dbHelp.setEditPageRow(pToA, pageData.get(Integer.parseInt(me.getKey().toString())).get_assessments_questions_id(), me.getValue().toString());
             }
             i.remove();
         }
