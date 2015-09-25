fe1b59112821efb46230f46ea6842199de0e5366
rossumg <rossumg@uw.edu>
2015-09-25 12:37:17 -0700

    add CreatePersonFragment, RecentFragment, SearchFragment

---
+++ app/src/main/java/com/surveyapp/rayce/surveyapp2/CreatePersonFragment.java
@@ -0,0 +1,182 @@
+package com.surveyapp.rayce.surveyapp2;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.Spinner;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CreatePersonFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CreatePersonFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CreatePersonFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    public static String TAG = "createPersonTag";
+
+    //private static final String ARG_PARAM1 = "param1";
+    //private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    //private String mParam1;
+    //private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+    private DBHelper dbHelp;
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     //* @param param1 Parameter 1.
+     //* @param param2 Parameter 2.
+     * @return A new instance of fragment CreatePersonFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    //  public static CreateFragment newInstance(String param1, String param2) {
+    public static CreatePersonFragment newInstance() {
+        CreatePersonFragment fragment = new CreatePersonFragment();
+        Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    // public CreatePersonFragment() {
+    // Required empty public constructor
+    //}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        dbHelp = new DBHelper(getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_create_person, container, false);
+
+        loadPersonIDDropdown(view);
+        //loadAssessmentTypeDropdown(view);
+
+        //nameView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {}
+
+    public void onNothingSelected(AdapterView<?> arg0) {}
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onListItemPressed(int position) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(position);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(int position);
+
+    }
+
+
+
+    public void loadPersonIDDropdown(View view) {
+
+        List<String> personIDs = dbHelp.getAllPersonIDs();
+        // convert to array
+        String[] stringArrayPersonID = new String[ personIDs.size() ];
+        personIDs.toArray(stringArrayPersonID);
+
+        final MultiAutoCompleteTextView dropdown = (MultiAutoCompleteTextView) view.findViewById(R.id.name);
+        dropdown.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, stringArrayPersonID);
+        dropdown.setThreshold(1);
+        dropdown.setAdapter(dataAdapter);
+
+        dropdown.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int index, long position) {
+                String text = dropdown.getText().toString();
+                Log.d("request!", "name selected: " + text);
+            }
+        });
+
+    }
+
+
+
+    public void loadAssessmentTypeDropdown(View view) {
+        Spinner dropdown = (Spinner) view.findViewById(R.id.assessment_type);
+        dropdown.setOnItemSelectedListener( new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                //Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+                Log.d("request!", "assessment_type item selected: " + item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d("request!", "spinner nothing selected");
+            }
+        });
+
+
+        List<String> assessmentTypes = dbHelp.getAllAssessmentTypes();
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, assessmentTypes);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(dataAdapter);
+
+    }
+
+}
